2d50fb6db3a7d0abe89d98b41286306a082cf914
Michael Sandula <msandula@gmail.com>
2016-02-25 16:45:33 -0500

    Got three of four buttons functioning properly

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ var app = angular.module('myModule', []);
 
 app.controller('myController', function(){
 	var main = this;
+	
 	// words list
 	main.words = [
 		'Boom', 
@@ -16,31 +17,24 @@ app.controller('myController', function(){
 		'Frazzled',
 		'Binoculars'
 	];
-	// function generates random word from list
-	var wordPicker = main.words[Math.floor(Math.random() * main.words.length)];
-	main.randomWord = function() {
-		return wordPicker;
-	};
-	// style classes for words
-	main.addStyleToWords = 'styleWords';
+
 	// phrases list
 	main.phrases = [
-		'Rock the casbah', 
-		'Go for it', 
-		'Keep on trucking', 
-		'Please try again', 
-		'I literally can\'t even', 
-		'That\'s what she said', 
-		'Keep it real',
-		'It\'s not you, it\'s me',
-		'Show me the money!',
-		'Why are you the way that you are?'
+		'What\'s another word for Thesaurus?', 
+		'You can\'t have everything. Where would you put it?', 
+		'Everywhere is within walking distance if you have the time.', 
+		'It\'s a small world, but I wouldn\'t want to have to paint it.', 
+		'A lot of people are afraid of heights. Not me, I\'m afraid of widths.', 
+		'Whenever I think of the past, it brings back so many memories.', 
+		'Curiosity killed the cat, but for a while I was a suspect',
+		'On the other hand, you have different fingers.',
+		'All those who believe in psychokinesis raise my hand.',
+		'If Barbie is so popular, why do you have to buy her friends?'
 	];
-	// function generates random phrase from list
-	var phrasePicker = main.phrases[Math.floor(Math.random() * main.phrases.length)];
-	main.randomPhrase = function() {
-		return phrasePicker;
-	};
+	
+	// style classes for words
+	main.addStyleToWords = 'styleWords';
+
 	// style classes for phrases
 	main.addStyleToPhrases = [
 		'stylePhrases1',
@@ -54,10 +48,35 @@ app.controller('myController', function(){
 		'stylePhrases9',
 		'stylePhrases10'
 	];
+
+	// function generates random word from list for p1
+	main.randomWords = function() {
+		var wordPicker = main.words[Math.floor(Math.random() * main.words.length)];
+		$("#display1").append(wordPicker + "<br />");
+	};
+
+	// function generates random word with style from list for p2
+	main.randomWordsWithStyle = function() {
+		var wordPicker = main.words[Math.floor(Math.random() * main.words.length)];
+		$("#display2").append(wordPicker + "<br />");
+	};
+
+	// function generates random phrase from list for p3
+	main.randomPhrases = function() {
+		var phrasePicker = main.phrases[Math.floor(Math.random() * main.phrases.length)];
+		$("#display3").append(phrasePicker + "<br />");
+	};
+
 	// function generates random class to apply to phrases
-	var stylePicker = main.addStyleToPhrases[Math.floor(Math.random() * main.addStyleToPhrases.length)];
 	main.randomClass = function() {
+		var stylePicker = main.addStyleToPhrases[Math.floor(Math.random() * main.addStyleToPhrases.length)];
 		return stylePicker;
+	};
+
+	// function generates random phrase with style from list for p4
+	main.randomPhrasesWithStyle = function() {
+		var phrasePicker = main.phrases[Math.floor(Math.random() * main.phrases.length)];
+		$("#display4").append(phrasePicker + "<br />");
 	};
 });
 
